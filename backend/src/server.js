468dce274991c6bfd912b480ecb6d7cc8ec687ae
brnaveen1307 <brnaveenkumar1307@gmail.com>
2025-10-02 22:15:08 +0530

    Backend completed and started with frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,24 +2,27 @@ import notesRoutes from './routes/notesRoutes.js'
 import express from 'express'
 import {connectDB} from './config/db.js'
 import dotenv from 'dotenv'
+import rateLimiter from './middleware/rateLimiter.js'
 
 dotenv.config()
 const PORT = process.env.PORT || 5001
 
 const app = express();
 app.use(express.json()) // this middleware will parse JSON bodies: req.body
+app.use(rateLimiter)
 
 // simple custom middleware
-app.use(function(req, res, next){
-    console.log(`Request Method is ${req.method} and Request URL is ${req.url}`)
-    next()
-})
+// app.use(function(req, res, next){
+//     console.log(`Request Method is ${req.method} and Request URL is ${req.url}`)
+//     next()
+// })
 
-connectDB();
+
 
 app.use("/api/notes", notesRoutes)
 
-app.listen(PORT, function(){
+connectDB().then(function(){
+    app.listen(PORT, function(){
     console.log("Server started on PORT: ", PORT)
 })
- 
+})
